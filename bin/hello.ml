d4ff50e72cf2f9599670d95065089aef1572b520
Łukasz Kurowski <crackcomm@gmail.com>
2020-10-22 06:32:54 +0200

    Update OCaml to 4.11.1000

--- bin/hello.ml
+++ bin/hello.ml
@@ -1,4 +1,7 @@
+(* Test *)
+
+(* Main *)
 let () =
-  Console.log @@ Printf.sprintf "test %B" Hello_ocaml_lib.Util.value;
+  Console.log @@ Printf.sprintf "test12sdf3 %B" Hello_ocaml_lib.Util.value;
   Window.my_alert "test"
 ;;
